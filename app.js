5d99a85a8a391f29f83e475aa51520d79b1dd8df
Shivani Gupta <shivani.gupta@paralleldots.com>
2023-06-11 20:20:58 +0530

    REST APIs, clients & CORS error

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 // app.use(bodyParser.urlencoded); // x-www-form-urlencoded  <form></form>
 app.use(bodyParser.json()); // application.json()
 
+//before sending any response, set headers
+app.use((req, res, next) => {
+  res.setHeader("Acess-Control-Allow-Origin", "*");
+  res.setHeader("Acess-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Acess-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // GET /feed/products
 app.use("/feed", feedRoutes);
 
