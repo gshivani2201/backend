9a203b6e1c6143c0cfffc088fa084b80c4d05fb1
Shivani Gupta <shivani.gupta@paralleldots.com>
2023-06-17 21:39:33 +0530

    transforming Then/Catch to Async/Await

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,36 +6,25 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
 
-exports.getposts = (req, res, next) => {
+exports.getposts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      if (!posts) {
-        const error = new Error("Could not find posts.");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({
-        posts: posts,
-        message: "Fetched posts successfully!",
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+    res.status(200).json({
+      posts: posts,
+      message: "Fetched posts successfully!",
+      totalItems: totalItems,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
